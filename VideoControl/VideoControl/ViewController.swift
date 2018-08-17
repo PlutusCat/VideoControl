7af542299cc05e6e6216207313693f52a9dff757
PlutusCat <haiyang_online@sina.com>
2018-08-17 09:51:10 +0800

    移除 Cocoapods

--- VideoControl/VideoControl/ViewController.swift
+++ VideoControl/VideoControl/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import IJKMediaFramework
 
 class ViewController: UIViewController {
 
+    var videoView: IJKFFMoviePlayerController!
+    var play: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+
+        let url = URL(string: "http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8")
+        let options = IJKFFOptions.byDefault()
+        videoView = IJKFFMoviePlayerController(contentURL: url, with: options)
+
+        let autoresize = UIViewAutoresizing.flexibleWidth.rawValue |
+            UIViewAutoresizing.flexibleHeight.rawValue
+        videoView.view.autoresizingMask = UIViewAutoresizing(rawValue: autoresize)
+
+        videoView.view.frame = view.bounds
+        videoView.scalingMode = .aspectFit
+        videoView.shouldAutoplay = true
+        videoView.prepareToPlay()
+
+        view.autoresizesSubviews = true
+        view.addSubview(videoView.view)
+
     }
 
     override func didReceiveMemoryWarning() {
